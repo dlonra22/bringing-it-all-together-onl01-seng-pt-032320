96f72a60de8313b5ffe46fb80b550106d7d542ef
arnie b <arnold.bwaila@gmail.com>
2020-06-11 19:26:08 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,4 +24,41 @@ class Dog
   def self.drop_table
     DB[:conn].execute("DROP TABLE IF EXISTS dogs")
   end
+  
+  def save
+    sql = <<-SQL
+          INSERT INTO dogs(name,breed)
+          VALUES(?,?)
+          SQL
+    DB[:conn].execute(sql,self.name,self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+  end
+  
+  def self.create(hash)
+    dog = Dog.new(name: hash[:name],breed: hash[:breed])
+    dog.save
+  end
+  
+  def self.new_from_db(row)
+    dog = Dog.new(id: row[0], name: row[1], breed: row[2])
+    dog
+  end
+  
+  def self.find_by_id(idy)
+      sql = <<-SQL 
+            SELECT * FROM dogs
+            WHERE id = ?
+            LIMIT 1 
+            SQL
+      DB[:conn].execute(sql,idy).collect do |row|
+        self.new_from_db(row)
+      end.first
+    end
+    
+  def self.find_or_create_by
+  end
+    
+    
+    
 end
